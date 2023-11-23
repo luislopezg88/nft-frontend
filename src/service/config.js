9e193cb4cf4bddc2bab4846d696c87375fed2a73
christopher-1988 <christopher_carnevale@outlook.com>
2023-11-23 18:02:12 -0500

    cambios

--- src/service/config.js
+++ src/service/config.js
@@ -1,7 +1,9 @@
 export const API_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3100/api"
-    : "vercel";
+    : "https://nft-backend-17tx7lhzc-lismarygoyo.vercel.app/api";
 
 export const IMG_URL =
-  process.env.NODE_ENV === "development" ? "http://localhost:3100" : "vercel";
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3100"
+    : "https://nft-backend-17tx7lhzc-lismarygoyo.vercel.app/api";
