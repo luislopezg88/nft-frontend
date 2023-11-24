ec70558fbe685669ab5643f87184e1606e3b82c6
LisPEx <lismarygoyo@gmail.com>
2023-11-23 20:48:03 -0500

    cambio de url de vercel

--- src/service/config.js
+++ src/service/config.js
@@ -1,9 +1,9 @@
 export const API_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3100/api"
-    : "https://nft-backend-phi.vercel.app";
+    : "https://nft-backend-phi.vercel.app/api";
 
 export const IMG_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3100"
-    : "https://nft-backend-phi.vercel.app";
+    : "https://nft-backend-phi.vercel.app/api";
